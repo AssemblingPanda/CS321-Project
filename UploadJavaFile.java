4e287b6417ef9150cd8dd924b2f02dfac8ca9a72
AssemblingPanda <71166544+AssemblingPanda@users.noreply.github.com>
2020-10-20 13:12:27 -0400

    Delete UploadJavaFile.java

--- UploadJavaFile.java
+++
@@ -1,6 +0,0 @@
-public class UploadJavaFile{
-	public static void main(String args []){
-		System.out.println("Everyone try uploading a Java file from your computer.");
-		System.out.println("Editing this file on GitHub");
-	}
-}
