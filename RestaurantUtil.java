e0b13c9ace46a70669d74b4405a5e22e090bd4b3
huitk <32004810+huitk@users.noreply.github.com>
2020-11-03 21:33:46 -0500

    Add files via upload

--- RestaurantUtil.java
+++ RestaurantUtil.java
@@ -1,102 +1,102 @@
-package	com.github.huitk;
+package com.github.huitk;
 //RestaurantUtil class
-public class RestaurantUtil	{
+public class RestaurantUtil {
 
-	//Get restaurant recommendations
-	//GoogleSearch's getResRec() calls has a large string with delimiter between each restaurant.
-	//Delimiter	is "XA Potential RestaurantX".
-	//When using split,	array index	0 is always	empty so restaurant	array is -1	the	split array.
-	//For each set of restaurant information batch in token	array, parse and get Restaurant	object back.
-	//If any Restaurants are null in Restaurant	array, then	make new array that	holds no nulls.
-	//Remove all duplicate Restaurants.
-	//Return the Restaurant	array or null if no	restaurants.
-	public static Restaurant[] getRecommendations(String criteria){
-		String searchResults = GoogleSearch.getResRec(criteria);
-		String[] tokens	= searchResults.split("XA Potential RestaurantX");
-		Restaurant[] recommendations = new Restaurant[tokens.length-1];
-		boolean	hasNull	= false;
-		int	numberOfNulls =	0;
-		for(int	i =	0; i < tokens.length-1;	i++){
-			recommendations[i] = getSetInformation(tokens[i+1]);
-			if(recommendations[i] == null){
-				hasNull	= true;
-				numberOfNulls++;
-			}
-		}
-		//remove nulls
-		if(hasNull == true && (numberOfNulls > 0)){
-			Restaurant[] tmp = new Restaurant[recommendations.length-numberOfNulls];
-			if(tmp.length == 0){
-				return null;
-			}
-			for(int	i =	0, j = 0; i	< recommendations.length &&	j <	tmp.length;	i++){
-				if(recommendations[i] != null){
-					tmp[j] = recommendations[i];
-					j++;
-				}
-			}
-			recommendations	= tmp;
-		}
-		//remove duplicates
-		numberOfNulls =	0;
-		for(int	i =	0; i < recommendations.length; i++){
-			for(int	j =	i+1; j < recommendations.length; j++){
-				if(recommendations[i] == null){
-					continue;
-				}
-				if(recommendations[i].equals(recommendations[j])){
-					recommendations[j] = null;
-					numberOfNulls++;
-					for(int	k =	j; k < recommendations.length-1; k++){
-						recommendations[k] = recommendations[k+1];
-						recommendations[k+1] = null;
-					}
-				}
-			}
-		}
-		if(numberOfNulls > 0){
-			Restaurant[] tmp = new Restaurant[recommendations.length-numberOfNulls];
-			for(int	i =	0; i < tmp.length; i++){
-				tmp[i] = recommendations[i];
-			}
-			recommendations	= tmp;
-		}
-		return recommendations;
-	}
-	
-	//Get a	recently opened	restaurant
-	//GoogleSearch's getRecentlyOpenedRes()	calls has a	large string with delimiter	between	each restaurant.
-	//Delimiter	is "XA Potential RestaurantX".
-	//When using split,	array index	0 is always	empty so restaurant	array is -1	the	split array.
-	//For each set of restaurant information batch in token	array, parse and get Restaurant	object back.
-	//If all is	null, then return null
-	//If there is Restaurant object, then use random to	pick one.
-	//Return a Restaurant or null if no	restaurants.
-	public static Restaurant findRecentlyOpened(String criteria){
-		String searchResults = GoogleSearch.getRecentlyOpenedRes(criteria);
-		String[] tokens	= searchResults.split("XA Potential RestaurantX");
-		Restaurant[] recentlyOpenedRes = new Restaurant[tokens.length-1];
-		int	numberOfRestaurants	= 0;
-		for(int	i =	0; i < recentlyOpenedRes.length; i++) {
-			Restaurant tmp = getSetInformation(tokens[i+1]);
-			if(tmp == null){
-				continue;
-			}
-			recentlyOpenedRes[numberOfRestaurants] = tmp;
-			numberOfRestaurants++;
-		}
-		if(numberOfRestaurants == 0){
-			return null;
-		}
-		else if(numberOfRestaurants	== 1){
-			return recentlyOpenedRes[0];
-		}
-		int	randomIndex	= (int)(Math.random()*2);
-		return recentlyOpenedRes[randomIndex];
-	}
-	
-	//Parse	allInfo	and	return Restaurant object with info
-	private	static Restaurant getSetInformation(String allInfo){
-		return new Restaurant();
-	}
+    //Get restaurant recommendations
+    //GoogleSearch's getResRec() calls has a large string with delimiter between each restaurant.
+    //Delimiter is "XA Potential RestaurantX".
+    //When using split, array index 0 is always empty so restaurant array is -1 the split array.
+    //For each set of restaurant information batch in token array, parse and get Restaurant object back.
+    //If any Restaurants are null in Restaurant array, then make new array that holds no nulls.
+    //Remove all duplicate Restaurants.
+    //Return the Restaurant array or null if no restaurants.
+    public static Restaurant[] getRecommendations(String criteria){
+        String searchResults = GoogleSearch.getResRec(criteria);
+        String[] tokens = searchResults.split("XA Potential RestaurantX");
+        Restaurant[] recommendations = new Restaurant[tokens.length-1];
+        boolean hasNull = false;
+        int numberOfNulls = 0;
+        for(int i = 0; i < tokens.length-1; i++){
+            recommendations[i] = getSetInformation(tokens[i+1]);
+            if(recommendations[i] == null){
+                hasNull = true;
+                numberOfNulls++;
+            }
+        }
+        //remove nulls
+        if(hasNull == true && (numberOfNulls > 0)){
+            Restaurant[] tmp = new Restaurant[recommendations.length-numberOfNulls];
+            if(tmp.length == 0){
+                return null;
+            }
+            for(int i = 0, j = 0; i < recommendations.length && j < tmp.length; i++){
+                if(recommendations[i] != null){
+                    tmp[j] = recommendations[i];
+                    j++;
+                }
+            }
+            recommendations = tmp;
+        }
+        //remove duplicates
+        numberOfNulls = 0;
+        for(int i = 0; i < recommendations.length; i++){
+            for(int j = i+1; j < recommendations.length; j++){
+                if(recommendations[i] == null){
+                    continue;
+                }
+                if(recommendations[i].equals(recommendations[j])){
+                    recommendations[j] = null;
+                    numberOfNulls++;
+                    for(int k = j; k < recommendations.length-1; k++){
+                        recommendations[k] = recommendations[k+1];
+                        recommendations[k+1] = null;
+                    }
+                }
+            }
+        }
+        if(numberOfNulls > 0){
+            Restaurant[] tmp = new Restaurant[recommendations.length-numberOfNulls];
+            for(int i = 0; i < tmp.length; i++){
+                tmp[i] = recommendations[i];
+            }
+            recommendations = tmp;
+        }
+        return recommendations;
+    }
+    
+    //Get a recently opened restaurant
+    //GoogleSearch's getRecentlyOpenedRes() calls has a large string with delimiter between each restaurant.
+    //Delimiter is "XA Potential RestaurantX".
+    //When using split, array index 0 is always empty so restaurant array is -1 the split array.
+    //For each set of restaurant information batch in token array, parse and get Restaurant object back.
+    //If all is null, then return null
+    //If there is Restaurant object, then use random to pick one.
+    //Return a Restaurant or null if no restaurants.
+    public static Restaurant findRecentlyOpened(String criteria){
+        String searchResults = GoogleSearch.getRecentlyOpenedRes(criteria);
+        String[] tokens = searchResults.split("XA Potential RestaurantX");
+        Restaurant[] recentlyOpenedRes = new Restaurant[tokens.length-1];
+        int numberOfRestaurants = 0;
+        for(int i = 0; i < recentlyOpenedRes.length; i++) {
+            Restaurant tmp = getSetInformation(tokens[i+1]);
+            if(tmp == null){
+                continue;
+            }
+            recentlyOpenedRes[numberOfRestaurants] = tmp;
+            numberOfRestaurants++;
+        }
+        if(numberOfRestaurants == 0){
+            return null;
+        }
+        else if(numberOfRestaurants == 1){
+            return recentlyOpenedRes[0];
+        }
+        int randomIndex = (int)(Math.random()*2);
+        return recentlyOpenedRes[randomIndex];
+    }
+    
+    //Parse allInfo and return Restaurant object with info
+    private static Restaurant getSetInformation(String allInfo){
+        return new Restaurant();
+    }
 }
