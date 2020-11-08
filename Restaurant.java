7fa75ae5041340899da225986bf53f42f84411be
AssemblingPanda <71166544+AssemblingPanda@users.noreply.github.com>
2020-11-07 20:40:30 -0500

    Add files via upload
    Almost complete. 
    Need some checks(in comments),
    Added some basic checks.

--- Restaurant.java
+++ Restaurant.java
@@ -1,145 +1,150 @@
-package com.github.huitk;
+package com.github.assemblingpanda;
+
 //Restaurant class
 public class Restaurant {
-    //declared variables
-    private String name;
-    private String numStars;
-    private String address;
-    private String numReviews;
-    private String restaurantType;
-    private String phoneNumber;
-    private String website;
-    private String operationalHours;
-    private String dollars;
-    
-    //default constructor set default value for instance variable
-    public Restaurant() {
-        name = "";
-        numStars = "";
-        address = "";
-        numReviews = "";
-        restaurantType = "";
-        phoneNumber = "";
-        website = "";
-        operationalHours = "";
-        dollars = "";
-    }
-    
-    //overloaded constructor that takes in values for all the instance variables
-    public Restaurant(String name, String numStars, String address, String numReviews, String dollars,
-            String restaurantType, String phoneNumber, String website, String operationalHours) {
-            this.name = name;
-            this.numStars = numStars;
-            this.address = address;
-            this.numReviews = numReviews;
-            this.restaurantType = restaurantType;
-            this.phoneNumber = phoneNumber;
-            this.website = website;
-            this.operationalHours = operationalHours;
-            this.dollars = dollars;
-    }
-    
-    
-    //mutator and accessor methods
-    //mutator or setter is used to set the values of the private field. 
-    //accessor or getter is used to return the values of the private field
+	//declared variables
+	private String name;
+	private String numStars;
+	private String address;
+	private String numReviews;
+	private String phoneNumber;
+	private String website;
+	private String operationalHours;
+	private String dollars;
+	
+	//default constructor set default value for instance variable
+	public Restaurant() {
+		name = "N/A";
+		numStars = "N/A";
+		address = "N/A";
+		numReviews = "N/A";
+		phoneNumber = "N/A";
+		website = "N/A";
+		operationalHours = "N/A";
+		dollars = "N/A";
+	}
 
-    public String getName() {
-        return name;
-    }
+	//overloaded constructor that takes in values for all the instance variables
+	public Restaurant(String name, String numStars, String address, String numReviews,
+					  String dollars, String phoneNumber, String website, String operationalHours) {
+		this.setName(name);
+		this.setNumStars(numStars);
+		this.setAddress(address);
+		this.setNumReviews(numReviews);
+		this.setDollars(dollars);
+		this.setPhoneNumber(phoneNumber);
+		this.setWebsite(website);
+		this.setOperationalHours(operationalHours);
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	
+	//mutator and accessor methods
+	//mutator or setter is used to set the values of the private field. 
+	//accessor or getter is used to return the values of the private field
 
-    public String getNumStars() {
-        return numStars;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setNumStars(String numStars) {
-        this.numStars = numStars;
-    }
+	public void setName(String name) {
+		if(name.equals("")) return;
+		this.name = name;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public String getNumStars() {
+		return numStars;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	// Check to make sure numStars is all numbers
+	public void setNumStars(String numStars) {
+		if(numStars.equals("")) return;
+		this.numStars = numStars;
+	}
 
-    public String getNumReviews() {
-        return numReviews;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public void setNumReviews(String numReviews) {
-        this.numReviews = numReviews;
-    }
+	public void setAddress(String address) {
+		if(address.equals("")) return;
+		if(address.contains("Hours")){
+			this.address = address.split("Hours")[0];
+			return;
+		}
+		this.address = address;
+	}
 
-    public String getRestaurantType() {
-        return restaurantType;
-    }
+	public String getNumReviews() {
+		return numReviews;
+	}
 
-    public void setRestaurantType(String restaurantType) {
-        this.restaurantType = restaurantType;
-    }
+	// Check to make sure numReviews is all numbers
+	public void setNumReviews(String numReviews) {
+		if(numReviews.equals("")) return;
+		this.numReviews = numReviews;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	// Check for all numbers, -, (, )
+	public void setPhoneNumber(String phoneNumber) {
+		if(phoneNumber.equals("")) return;
+		this.phoneNumber = phoneNumber;
+	}
 
-    public String getWebsite() {
-        return website;
-    }
+	public String getWebsite() {
+		return website;
+	}
 
-    public void setWebsite(String website) {
-        this.website = website;
-    }
+	public void setWebsite(String website) {
+		if(website.equals("")) return;
+		this.website = website;
+	}
 
-    public String getOperationalHours() {
-        return operationalHours;
-    }
+	public String getOperationalHours() {
+		return operationalHours;
+	}
 
-    public void setOperationalHours(String operationalHours) {
-        this.operationalHours = operationalHours;
-    }
+	// make it look better, format the structure
+	public void setOperationalHours(String operationalHours) {
+		if(operationalHours.equals("")) return;
+		this.operationalHours = operationalHours;
+	}
 
-    public String getDollars(){
-        return dollars;
-    }
+	public String getDollars(){
+		return dollars;
+	}
 
-    public void setDollars(String dollars){
-        this.dollars = dollars;
-    }
+	public void setDollars(String dollars){
+		if(dollars.equals("")) return;
+		this.dollars = dollars;
+	}
 
-    //ToString method return the string represent in the object. It is used to print the object as string.
-    public String toString() {
-        return "Name: " + name + "\n"
-                + "Address: " + address + "\n"
-                + "Ratings: " + numStars + "\n"
-                + "Number of Reviews: " + numReviews + "\n"
-                + "Cost: " + dollars + "\n"
-                + "Restaurant Type: " + restaurantType + "\n"
-                + "Phone Number: " + phoneNumber + "\n"
-                + "Website: " + "" + website + "\n"
-                + "Today's operational Hours: " + operationalHours + "\n";
-    }
-    
-    //Two restaurants are equals if and only if they have the same name and same address
-    public boolean equals(Object otherObject) {
-        //set isEqual to false. If name and address of the restaurant are the same, isEqual will be true.
-        boolean isEqual = false;
-        if(otherObject != null && otherObject instanceof Restaurant) {
-            Restaurant otherRestaurant = (Restaurant)otherObject;
-            if(super.equals(otherRestaurant) && this.name.equals(otherRestaurant.name) 
-                    && this.address.equals(otherRestaurant.address)){
-                    isEqual = true;
-            }
-        }
-        return isEqual;
-    }
+	//ToString method return the string represent in the object. It is used to print the object as string.
+	public String toString() {
+		return "Name: " + name + "\n"
+				+ "Address: " + address + "\n"
+				+ "Ratings: " + numStars + "\n"
+				+ "Number of Reviews: " + numReviews + "\n"
+				+ "Cost: " + dollars + "\n"
+				+ "Phone Number: " + phoneNumber + "\n"
+				+ "Website: " + website + "\n"
+				+ "Today's operational Hours: " + operationalHours + "\n";
+	}
+	
+	//Two restaurants are equals if and only if they have the same name and same address
+	public boolean equals(Object otherObject) {
+		//set isEqual to false. If name and address of the restaurant are the same, isEqual will be true.
+		boolean isEqual = false;
+		if(otherObject != null && otherObject instanceof Restaurant) {
+			Restaurant otherRestaurant = (Restaurant)otherObject;
+			if(super.equals(otherRestaurant) && this.name.equals(otherRestaurant.name) 
+					&& this.address.equals(otherRestaurant.address)){
+					isEqual = true;
+			}
+		}
+		return isEqual;
+	}
 }
